ccd27ee506e0c562cec9988a8560d13c0c5615e2
wishyoudie <66038379+wishyoudie@users.noreply.github.com>
2021-11-18 23:59:51 +0300

    OpenHashSet class solved

--- src/lesson12/task1/OpenHashSet.kt
+++ src/lesson12/task1/OpenHashSet.kt
@@ -25,28 +25,73 @@ class OpenHashSet<T>(val capacity: Int) {
     /**
      * Число элементов в хеш-таблице
      */
-    val size: Int get() = TODO()
+    val size: Int
+        get() {
+            var res = 0
+            for (el in elements)
+                when (el) {
+                    null -> continue
+                    is MutableList<*> -> res += el.size
+                    else -> res++
+                }
+            return res
+        }
 
     /**
      * Признак пустоты
      */
-    fun isEmpty(): Boolean = TODO()
+    fun isEmpty(): Boolean = this.size == 0
 
     /**
      * Добавление элемента.
      * Вернуть true, если элемент был успешно добавлен,
      * или false, если такой элемент уже был в таблице, или превышена вместимость таблицы.
      */
-    fun add(element: T): Boolean = TODO()
+    fun add(element: T): Boolean {
+        if (element in this || this.size == capacity) return false
+        val i = element.hashCode() % capacity
+        when (val suspect = elements[i]) {
+            null -> elements[i] = element
+            is MutableList<*> -> elements[i] = mutableListOf(element) + suspect
+            else -> elements[i] = mutableListOf(suspect, element)
+        }
+        return true
+    }
 
     /**
      * Проверка, входит ли заданный элемент в хеш-таблицу
      */
-    operator fun contains(element: T): Boolean = TODO()
+    operator fun contains(element: T): Boolean {
+        val i = element.hashCode() % capacity
+        when (val suspect = elements[i]) {
+            null -> return false
+            is MutableList<*> -> element in suspect
+            else -> return suspect == element
+        }
+        return true
+    }
 
     /**
      * Таблицы равны, если в них одинаковое количество элементов,
      * и любой элемент из второй таблицы входит также и в первую
      */
-    override fun equals(other: Any?): Boolean = TODO()
+    override fun equals(other: Any?): Boolean {
+        if (other is OpenHashSet<*> && other.size == this.size) {
+            for (el in other.elements)
+                when (el) {
+                    null -> continue
+                    is MutableList<*> -> if (el.any { it as T !in this }) return false
+                    else -> if (el as T !in this) return false
+                }
+            return true
+        } else {
+            return false
+        }
+    }
+
+    override fun hashCode(): Int {
+        var res = 0
+        for (el in elements) if (el != null) res += el.hashCode()
+        return res
+    }
 }
